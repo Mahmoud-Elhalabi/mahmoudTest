ba289c78a036c829b97523251a0e68c16b816335
Mahmoud-Elhalabi <Mahmoudhalabi2019@gmail.com>
2020-05-28 13:18:54 +0300

    mahmoud

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+var button1 = document.getElementById("Profile")
+button1.onclick = function (){
+	
+
+} 
+var button2 = document.getElementById("Sports")
+button2.onclick = function (){
+	
+
+} 
+var button3 = document.getElementById("Workout Posts")
+button3.onclick = function (){
+	
+
+} 
+var button4 = document.getElementById("Weekly Total")
+button4.onclick = function (){
+	
+
+} 
+var button5 = document.getElementById("Friends")
+button5.onclick = function (){
+	
+
+}
